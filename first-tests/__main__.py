c8dd03803d6da6ade1bb93c974c77a1aa50f33f3
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2018-11-17 01:47:33 -0200

    Reworking as dumb as possible.

--- first-tests/__main__.py
+++ first-tests/__main__.py
@@ -1,85 +1,149 @@
-from sys import argv
-from typing import List
-
 import numpy as np
+from defaultlist import defaultlist as dlist
+
+from sys import argv
+from typing import Tuple
+import csv
 
 
-def sigmoid(x):
-    return 1.0/(1+ np.exp(-x))
+def sigmoid(x: float) -> float:
+    return 1 / (1 + np.exp(-x))
 
 
-def sigmoid_derivative(x):
-    return x * (1.0 - x)
+def sigmoid_derivative(x: float) -> float:
+    return x * (1 - x)
+
+
+def make_layer(input_length: int, neurons: int):
+    weights = np.random.rand(neurons, input_length) * 2 - 1
+    biases = np.random.rand(neurons) * 2 - 1
+    return weights, biases
 
 
 class NeuralNetwork:
-    def __init__(self, x: List[List[int]], y: List[List[int]]):
-        print(f'len(x): {len(x)}\nlen(y): {len(y)}')
-        self.input: List[List[int]] = x
-        self.weights: List[List[List[int]]] = [
-            np.random.rand(self.input.shape[1], len(y)),
-            np.random.rand(len(y), 1),
-        ]
-        self.y: List[List[int]] = y
-        self.output: List[int] = np.zeros(y.shape)
+    def __init__(
+        self,
+        input_length,
+        hidden_layers,
+        neurons_per_layer,
+        output_length,
+        activate,
+        activate_diff,
+    ):
+        first_layer = make_layer(input_length, neurons_per_layer)[0]
+        hidden_layers = (
+            make_layer(neurons_per_layer, neurons_per_layer)[0]
+            for _ in range(hidden_layers - 1)
+        )
+        output_layer = make_layer(output_length, neurons_per_layer)[0]
 
-    def feedforward(self):
-        self.layer1 = sigmoid(np.dot(self.input, self.weights[0]))
-        self.output = sigmoid(np.dot(self.layer1, self.weights[1]))
+        self.layers = [first_layer, *hidden_layers, output_layer]
+        self.activate = activate
+        self.activate_diff = activate_diff
 
-    def backprop(self):
-        d_weights = [
-            np.dot(
-                self.input.T,
-                (np.dot(
-                    2*(self.y - self.output) * sigmoid_derivative(self.output),
-                    self.weights[1].T
-                ) * sigmoid_derivative(self.layer1))
-            ),
-            np.dot(
-                self.layer1.T,
-                (2*(self.y - self.output) * sigmoid_derivative(self.output))
-            ),
-        ]
+    def train(self, input_row, expected_output):
+        '''
+        HINT: A Perceptron is defined by a weights vector.
+        '''
+        # Setup
+        input_layer, *hidden_layers, output_layer = self.layers
 
-        self.weights = [
-            w + d_w for w, d_w in zip(self.weights, d_weights)
-        ]
+        # Feed forward
+        print('Feed me >:) Feeding...')
+
+        # -> Input layer
+        a = dlist(lambda *a: dlist(lambda *b: float))
+        z = dlist(lambda *a: dlist(lambda *b: float))
+        for j, weights in enumerate(input_layer):
+            z[0][j] = sum(i*weights[k]
+                          for k, i in enumerate(input_row))
+            a[0][j] = self.activate(z[0][j])
+
+        # -> Hidden layers
+        for l, layer in enumerate(hidden_layers):
+            last_layer = self.layers[l]
+            for j, weights in enumerate(self.layers[l+1]):
+                z[l+1][j] = sum(a*weights[k]
+                                for k, a in enumerate(last_layer))
+                a[l+1][j] = self.activate(z[l][j])
+
+        # -> Output layer
+        for j, weights in enumerate(output_layer):
+            _sum = 0
+            for k, i in enumerate(a[l][j]):
+                print('-'*80)
+                print(f'i: {i}')
+                print(f'w: {weights[k]}')
+                _sum += i*weights[k]
+            z[l+1][j] = _sum
+            a[l+1][j] = self.activate(z[l+1][j])
+
+        # Backpropagation
+        print('Backpropping...')
+
+        # -> C(o)
+        print(f'answ: {list(a[-1])}')
+        print(f'expc: {expected_output}')
+        error = sum(np.square(result - expected)
+                    for result, expected in zip(a[-1], expected_output))
+        print(f'error: {error}')
+
+        # TODO: Keep going with the backprop, bro :)
+
+
+def read_dataset(csv_path: str):
+    with open(csv_path) as f:
+        reader = csv.reader(f)
+        next(reader)  # skip header
+
+        for label, *pixels in reader:
+            pixels = np.array(pixels).astype(np.float) / 255
+            # normalized to [0, 1]
+
+            output = np.zeros(10)
+            output[int(label)] = 1
+
+            yield pixels, output
+
+
+def cli():
+    usage = (
+        f'Usage: {argv[0]} '
+        '<training-dataset> '
+        '<hidden-layers> '
+        '<neurons-per-layer>'
+    )
+
+    if '-h' in argv or '--help' in argv:
+        print(usage)
+        exit(0)
+
+    try:
+        _, training_dataset, hidden_layers, neurons_per_layer = argv
+        hidden_layers = int(hidden_layers)
+        neurons_per_layer = int(neurons_per_layer)
+    except (ValueError, TypeError):
+        print(usage)
+        exit(1)
+
+    return read_dataset(training_dataset), hidden_layers, neurons_per_layer
+
+
+def main():
+    training_dataset, hidden_layers, neurons_per_layer = cli()
+
+    net = NeuralNetwork(
+        input_length=784,
+        hidden_layers=hidden_layers,
+        neurons_per_layer=neurons_per_layer,
+        output_length=10,
+        activate=sigmoid,
+        activate_diff=sigmoid_derivative,
+    )
+
+    for pixels, label in training_dataset:
+        net.train(pixels, label)
 
 
 if __name__ == '__main__':
-    try:
-        train_file = argv[1]
-        trains = argv[2]
-    except IndexError:
-        print(f'Usage: {argv[0]} <training file> <train iter count>')
-        exit(1)
-
-    print('Gathering data...')
-
-    inputs = []
-    outputs = []
-    with open(train_file) as f:
-        labels = f.readline().split(',')
-        print(f'Labels: {labels}')
-
-        for line in f.readlines():
-            output, *data = line.split(',')
-            inputs += [[int(data) for data in data]]
-            outputs += [output]
-
-    inputs = np.array(inputs)
-    outputs = np.array(outputs)
-
-    print('Creating NeuroNet...')
-    neuro = NeuralNetwork(inputs, outputs)
-
-    print(f'Training {trains} times...')
-    try:
-        for i in range(1500):
-            neuro.feedforward()
-            neuro.backprop()
-    except MemoryError:
-        print(f'Failed in iteration {i}')
-
-    print(neuro.output)
+    main()
